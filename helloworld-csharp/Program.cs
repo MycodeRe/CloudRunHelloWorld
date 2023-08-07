33efbe67a890a17b18a3a0758ad36220fc2ea275
Priya Venkatesan <priya.a.venkatesan@accenture.com>
2023-08-07 10:39:03 +0530

    triggerntest

--- helloworld-csharp/Program.cs
+++ helloworld-csharp/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World Priyaaaaa!");
+app.MapGet("/", () => "Hello World Priyaaaaa 12323233!");
 
 app.Run();
